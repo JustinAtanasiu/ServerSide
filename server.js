f262c24149d69cf59af93020c846346416f1452b
JustinAtanasiu <justin.atanasiu@gmail.com>
2016-06-05 01:29:19 +0300

    token based works, need location weather api to secure with token, encrypt decrypt db, ssl

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var jwt = require('jsonwebtoken'); 
 var config = require('./config');
-
+var crypto = require('crypto');
 
 var persAssist = nano.db.use('personalassistant');
 var port = 8080;
@@ -25,14 +25,7 @@ app.listen(port);
 console.log('Connected at http://localhost:' + port);
 
 var apiRoutes = express.Router();
- 
-// route to return all users (GET http://localhost:8080/api/users)
-apiRoutes.get('/users', function (req, res) {
-    persAssist.list({include_docs: true}, function (err, response) {
-        res.json(response.rows);
-    });
-});  
- 
+
 apiRoutes.post('/signUp', function (req, res) { 
     var data = req.body;
     res.send('Added:' + data);
@@ -42,6 +35,86 @@ apiRoutes.post('/signUp', function (req, res) {
         console.log('User saved successfully');
     });
 }); 
+
+apiRoutes.post('/checkUsers', function (req, res) {
+    var testUsername = false;
+    persAssist.list({include_docs: true}, function (err, response) {
+        (response.rows).forEach(function(element) {
+            if(element.doc.username === req.body.username){
+                res.send({status:400});
+                testUsername = true;
+            }
+        }, this);        
+        if(!testUsername)
+            res.send({status:200});
+    });
+});
+
+apiRoutes.post('/authenticate', function (req, res) { 
+   persAssist.list({include_docs: true}, function (err, response) {
+       var foundUser = false;
+       response.rows.forEach(function(element) {
+           if(element.doc.username === req.body.username && element.doc.password === req.body.password){
+               foundUser = element.doc;
+           }
+       }, this);
+       if(foundUser){
+           var token = jwt.sign({id: foundUser._id}, app.get('superSecret'), {
+               expiresIn: 86400 // expires in 24 hours
+           });
+
+           // return the information including token as JSON
+           res.json({
+               success: true,
+               message: 'Enjoy your token!',
+               token: token,
+               id: foundUser._id
+           });
+       }
+       else{
+            res.json({
+               success: false
+           });
+       }
+    });
+}); 
+
+apiRoutes.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+ 
+apiRoutes.get('/users', function (req, res) {
+    persAssist.list({include_docs: true}, function (err, response) {
+        //all users
+    });
+});  
 
 apiRoutes.get('/getInfo/:id', function (req, res) { 
     var id = req.url.split("/")[2];
@@ -62,25 +135,12 @@ apiRoutes.post('/saveInfo/:id', function (req, res) {
         req.body.password = bodyTwo.password;
         req.body.username = bodyTwo.username;
         persAssist.insert(req.body, id, function (err, body) {
-        if (!err)
-            console.log(body)
-    });
+            if (!err)
+                console.log(body)
+        });
     });
     
 });
 
-apiRoutes.post('/checkUsers', function (req, res) {
-    var testUsername = false;
-    persAssist.list({include_docs: true}, function (err, response) {
-        (response.rows).forEach(function(element) {
-            if(element.doc.username === req.body.username){
-                res.send({status:400});
-                testUsername = true;
-            }
-        }, this);        
-        if(!testUsername)
-            res.send({status:200});
-    });
-});
 // apply the routes to our application with the prefix /api
 app.use('/api', apiRoutes);
